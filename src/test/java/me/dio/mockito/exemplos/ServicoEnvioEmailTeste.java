735d86582f9e052b9ad8d80e9b6d050e7aebc657
ssperandio <dev.ssperandio@gmail.com>
2024-08-07 11:43:30 -0300

    Capturando Argumentos

---
+++ src/test/java/me/dio/mockito/exemplos/ServicoEnvioEmailTeste.java
@@ -0,0 +1,39 @@
+package me.dio.mockito.exemplos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ServicoEnvioEmailTeste {
+
+    @Mock
+    private PlataformaDeEnvio plataforma;
+
+    @InjectMocks
+    private ServicoEnvioEmail servico;
+
+    @Captor
+    private ArgumentCaptor<Email> captor;
+
+    @Test
+    void validarDadosEnviadosParaAPlataforma() {
+        String enderecoDeEmail = "ssperandio@test.com";
+        String mensagem = "Mensagem para teste de e-mail";
+        boolean ehFormatoHtml = false;
+
+        servico.enviaEmail(enderecoDeEmail, mensagem, ehFormatoHtml);
+
+        Mockito.verify(plataforma).enviaEmail(captor.capture());
+
+        Email emailCapturado = captor.getValue();
+
+        Assertions.assertEquals(enderecoDeEmail, emailCapturado.getEnderecoEmail());
+        Assertions.assertEquals(mensagem, emailCapturado.getMensagem());
+        Assertions.assertEquals(Formato.TEXTO, emailCapturado.getFormato());
+    }
+
+
+}
